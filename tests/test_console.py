b742bcc0befadbeb756474108921b3bed84eb911
Pekka Röyttä <pekka.roytta@spectralengines.com>
2020-03-28 11:16:21 +0200

    uses poetry pytest coverage

---
+++ tests/test_console.py
@@ -0,0 +1,29 @@
+import click.testing
+import pytest
+
+from hmtest import console
+
+@pytest.fixture
+def runner():
+    return click.testing.CliRunner()
+
+@pytest.fixture
+def mock_requests_get(mocker):
+    mock = mocker.patch("requests.get")
+    mock.return_value.__enter__.return_value.json.return_value = {
+        "title": "Lorem Ipsum",
+        "extract": "Lorem ipsum dolor sit amet",
+    }
+    return mock
+
+def test_main_succeeds(runner, mock_requests_get):
+    result = runner.invoke(console.main)
+    assert result.exit_code == 0
+
+def test_main_prints_title(runner, mock_requests_get):
+    result = runner.invoke(console.main)
+    assert "Lorem Ipsum" in result.output
+
+def test_main_invokes_requests_get(runner, mock_requests_get):
+    _ = runner.invoke(console.main)
+    assert mock_requests_get.called
