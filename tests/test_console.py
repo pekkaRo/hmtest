3a014cdf9969219d798502898ef72aea2ab9de58
Pekka Röyttä <pekka.roytta@spectralengines.com>
2020-03-28 12:50:24 +0200

    add language selection option and flag

--- tests/test_console.py
+++ tests/test_console.py
@@ -8,6 +8,10 @@ from hmtest import console
 def runner():
     return click.testing.CliRunner()
 
+@pytest.fixture
+def mock_wikipedia_random_page(mocker):
+    return mocker.patch("hmtest.wikipedia.random_page")
+
 
 def test_main_succeeds(runner, mock_requests_get):
     result = runner.invoke(console.main)
@@ -35,3 +39,7 @@ def test_main_prints_message_on_request_error(runner, mock_requests_get):
     mock_requests_get.side_effect = requests.RequestException
     result = runner.invoke(console.main)
     assert "Error" in result.output
+
+def test_main_uses_specified_language(runner, mock_wikipedia_random_page):
+    runner.invoke(console.main, ["--language=pl"])
+    mock_wikipedia_random_page.assert_called_with(language="pl")
---
+++ tests/test_wikipedia.py
@@ -0,0 +1,6 @@
+from hmtest import wikipedia
+
+def test_random_page_uses_given_language(mock_requests_get):
+    wikipedia.random_page(language='fi')
+    args, _ = mock_requests_get.call_args
+    assert "fi.wikipedia.org" in args[0]
